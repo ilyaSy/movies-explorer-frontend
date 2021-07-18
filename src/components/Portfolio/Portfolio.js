244e9b4d2da2cae27f7139844514b92f642d25bb
Ilya Sychugov <ilya.sychugov@gmail.com>
2021-07-18 19:50:12 +0300

    Feat: portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,5 +1,31 @@
+import arrowPic from '../../images/arrowDiag.svg';
 import './Portfolio.css';
 
 export default function Portfolio(){
-  return (<></>);
+  return (
+    <section className='portfolio'>
+      <h3 className='portfolio__title'>Портфолио</h3>
+
+      <ul className='portfolio__links'>
+        <li className='portfolio__links-item'>
+          <a href='' rel='noreferer' alt='Статичный сайт' className='portfolio__link'>
+            Статичный сайт
+            <img src={arrowPic} alt='Стрелка переход на сайт' />
+          </a>
+        </li>
+        <li className='portfolio__links-item'>
+          <a href='' rel='noreferer' alt='Адаптивный сайт' className='portfolio__link'>
+            Адаптивный сайт
+            <img src={arrowPic} alt='Стрелка переход на сайт' />
+          </a>
+        </li>
+        <li className='portfolio__links-item'>
+          <a href='' rel='noreferer' alt='SPA' className='portfolio__link'>
+            Одностраничное приложение
+            <img src={arrowPic} alt='Стрелка переход на сайт' />
+          </a>
+        </li>
+      </ul>
+    </section>
+  );
 }
