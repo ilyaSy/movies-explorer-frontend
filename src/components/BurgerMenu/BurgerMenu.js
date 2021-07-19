1e02e0732bd79e562aa318adc8bdd35df0c63871
Ilya Sychugov <ilya.sychugov@gmail.com>
2021-07-20 00:14:38 +0300

    Fix: burger

--- src/components/BurgerMenu/BurgerMenu.js
+++ src/components/BurgerMenu/BurgerMenu.js
@@ -1,22 +1,54 @@
 import { NavLink } from 'react-router-dom';
 import { profileURL, moviesURL, savedMoviesURL } from '../../utils/constants';
 import accountImage from '../../images/account.svg';
+import burgerCloseImage from '../../images/burgerClose.svg';
 import './BurgerMenu.css';
 
-export default function BurgerMenu ({isOpened, onClose}) {
+export default function BurgerMenu({ isOpened, onClose }) {
   const handleCloseOverlay = (event) => {
     if (isOpened && event.target.classList.contains('burger_opened')) {
       onClose();
     }
-  }
+  };
 
   return (
-    <div className={`burger ${isOpened && 'burger_opened'}`} onClick={handleCloseOverlay}>
+    <div
+      className={`burger ${isOpened && 'burger_opened'}`}
+      onClick={handleCloseOverlay}
+    >
       <nav className='burger__menu-list'>
+        <button className='burger__button-close' onClick={onClose}>
+          <img src={burgerCloseImage} alt='Закрыть меню' className='burger__button-close-image'/>
+        </button>
         <ul className='burger__menu'>
-          <li><NavLink to={moviesURL} className='burger__menu-link'>Главная</NavLink></li>
-          <li><NavLink to={moviesURL} className='burger__menu-link'>Фильмы</NavLink></li>
-          <li><NavLink to={savedMoviesURL} className='burger__menu-link'>Сохранённые фильмы</NavLink></li>
+          <li className='burger__menu-items'>
+            <NavLink
+              exact
+              to='/'
+              className='burger__menu-link'
+              activeClassName='burger__menu-link_underlined'
+            >
+              Главная
+            </NavLink>
+          </li>
+          <li className='burger__menu-items'>
+            <NavLink
+              to={moviesURL}
+              className='burger__menu-link'
+              activeClassName='burger__menu-link_underlined'
+            >
+              Фильмы
+            </NavLink>
+          </li>
+          <li className='burger__menu-items'>
+            <NavLink
+              to={savedMoviesURL}
+              className='burger__menu-link'
+              activeClassName='burger__menu-link_underlined'
+            >
+              Сохранённые фильмы
+            </NavLink>
+          </li>
         </ul>
 
         <NavLink to={profileURL} alt='Профиль' className='burger__account'>
@@ -26,8 +58,8 @@ export default function BurgerMenu ({isOpened, onClose}) {
             alt='circle'
             className='burger__account-logo'
           />
-        </NavLink >
+        </NavLink>
       </nav>
     </div>
-  )
+  );
 }
