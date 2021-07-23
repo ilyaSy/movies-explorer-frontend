eff9cc82919d245e9e75aa52054caff552eafca0
Ilya Sychugov <ilya.sychugov@gmail.com>
2021-07-23 20:35:09 +0300

    Feat: preloader

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -1,13 +1,29 @@
+import { useEffect, useState } from 'react';
 import SearchForm from '../SearchForm/SearchForm';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import { movies } from '../../utils/testData';
+import Preloader from '../Preloader/Preloader';
 import './SavedMovies.css';
 
 export default function Movies () {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoaded(true);
+    }, 1000);
+  }, []);
+
   return (
     <main className='saved-movies'>
       <SearchForm />
-      <MoviesCardList movies={movies} />
+      {
+        isLoaded ? (
+          <MoviesCardList movies={movies} />
+        ) : (
+          <Preloader />
+        )
+      }
     </main>
   );
 }
