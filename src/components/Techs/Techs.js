b511ef6eb18e83954958bf44f625c211e1ee60d2
Ilya Sychugov <ilya.sychugov@gmail.com>
2021-07-24 22:32:45 +0300

    Fixes

--- src/components/Techs/Techs.js
+++ src/components/Techs/Techs.js
@@ -11,15 +11,6 @@ export default function Techs() {
         На курсе веб-разработки мы освоили технологии, которые применили в
         дипломном проекте.
       </p>
-      {/* <ul className='techs__list'>
-        <li className='techs__list-item'>HTML</li>
-        <li className='techs__list-item'>CSS</li>
-        <li className='techs__list-item'>JS</li>
-        <li className='techs__list-item'>React</li>
-        <li className='techs__list-item'>Git</li>
-        <li className='techs__list-item'>Express.js</li>
-        <li className='techs__list-item'>mongoDB</li>
-      </ul> */}
 
       <div className='techs__list'>
         <p className='techs__list-item'>HTML</p>
