f02832eed87fb9305b085d009169b2189930cb3e
Ilya Sychugov <ilya.sychugov@gmail.com>
2021-07-24 22:25:40 +0300

    Feat: adaptive techs

--- src/components/Techs/Techs.js
+++ src/components/Techs/Techs.js
@@ -11,7 +11,7 @@ export default function Techs() {
         На курсе веб-разработки мы освоили технологии, которые применили в
         дипломном проекте.
       </p>
-      <ul className='techs__list'>
+      {/* <ul className='techs__list'>
         <li className='techs__list-item'>HTML</li>
         <li className='techs__list-item'>CSS</li>
         <li className='techs__list-item'>JS</li>
@@ -19,7 +19,17 @@ export default function Techs() {
         <li className='techs__list-item'>Git</li>
         <li className='techs__list-item'>Express.js</li>
         <li className='techs__list-item'>mongoDB</li>
-      </ul>
+      </ul> */}
+
+      <div className='techs__list'>
+        <p className='techs__list-item'>HTML</p>
+        <p className='techs__list-item'>CSS</p>
+        <p className='techs__list-item'>JS</p>
+        <p className='techs__list-item'>React</p>
+        <p className='techs__list-item'>Git</p>
+        <p className='techs__list-item'>Express.js</p>
+        <p className='techs__list-item'>mongoDB</p>
+      </div>
     </section>
   );
 }
