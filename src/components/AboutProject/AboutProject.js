77ed421cf7ee1875000d3ebf4a5a2822cc271050
Ilya Sychugov <ilya.sychugov@gmail.com>
2021-07-24 18:36:03 +0300

    Feat: adaptive project

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -1,16 +1,26 @@
 import MainHeader from '../MainHeader/MainHeader';
 import './AboutProject.css';
 
-export default function AboutProject(){
+export default function AboutProject() {
   return (
     <section id='project' className='project'>
       <MainHeader title='О проекте' />
 
       <div className='project__details'>
-        <h3 className='project__details-title'>Дипломный проект включал 5 этапов</h3>
-        <h3 className='project__details-title'>На выполнение диплома ушло 5 недель</h3>
-        <p className='project__details-text'>Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
-        <p className='project__details-text'>У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+        <h3 className='project__details-title project__details-title_t1'>
+          Дипломный проект включал 5 этапов
+        </h3>
+        <h3 className='project__details-title project__details-title_t2'>
+          На выполнение диплома ушло 5 недель
+        </h3>
+        <p className='project__details-text project__details-text_t1'>
+          Составление плана, работу над бэкендом, вёрстку, добавление
+          функциональности и финальные доработки.
+        </p>
+        <p className='project__details-text project__details-text_t2'>
+          У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
+          соблюдать, чтобы успешно защититься.
+        </p>
       </div>
 
       <div className='project__progress'>
